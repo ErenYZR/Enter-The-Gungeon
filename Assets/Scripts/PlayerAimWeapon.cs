03a46a2cd7e0a73848d65b6d2f20b2f41c59bce5
ErenYZR <erenyazar16@gmail.com>
2025-02-16 17:43:09 +0300

    Firing

---
+++ Assets/Scripts/PlayerAimWeapon.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAimWeapon : MonoBehaviour
+{
+	public event EventHandler<OnShootEventArgs> OnShoot;
+
+	public class OnShootEventArgs : EventArgs
+	{
+		public Vector3 gunEndPosition;
+		public Vector3 shootPosition;
+	}
+
+	private Transform aimTransform;
+	public Camera cam;
+	private Vector3 mousePos;
+	private Transform aimGunEndPositionTransform;
+	[SerializeField] private GameObject bulletPrefab;
+
+	private Animator aimAnimator;
+
+	private void Awake()
+	{
+		aimTransform = transform.Find("Aim");
+		aimAnimator = aimTransform.GetComponent<Animator>();
+		aimGunEndPositionTransform = aimTransform.Find("GunEndPointPosition");
+	}
+
+	private void Update()
+	{
+		HandleAiming();
+		HandleShooting();
+	}
+
+	private void HandleAiming()
+	{
+		mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+		mousePos.z = 0f;
+
+		Vector3 aimDirection = (mousePos - transform.position).normalized;
+		float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+		aimTransform.eulerAngles = new Vector3(0, 0, angle);
+	}
+
+	private void HandleShooting()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			aimAnimator.SetTrigger("Shoot");
+
+			OnShoot?.Invoke(this, new OnShootEventArgs {
+				gunEndPosition = aimGunEndPositionTransform.position,
+				shootPosition = mousePos,
+			});
+
+			Instantiate(bulletPrefab, aimGunEndPositionTransform.position, aimGunEndPositionTransform.rotation);
+		}
+	}
+}
